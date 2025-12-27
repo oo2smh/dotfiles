7a228183f0099e4aa8de7751e06422bc5fb0152e
oo2smh <haminson@gmail.com>
2025-12-27 11:59:38 -0800

    Fix hyprland settings to better match default Windows keybinds

--- hypr/toggle_monitors.sh
+++ hypr/toggle_monitors.sh
@@ -3,11 +3,23 @@
 # allows for toggling between 2 monitors with 1 keybind in
 
 # Get the current active monitor
-current_monitor=$(hyprctl activeworkspace -j | jq -r '.monitor')
+CURRENT_MONITOR=$(hyprctl activeworkspace -j | jq -r '.monitor')
 
-# Toggle between HDMI-A-1 and DP-3
-if [[ "$current_monitor" == "HDMI-A-1" ]]; then
-    hyprctl dispatch focusmonitor "DP-3"
-else
-    hyprctl dispatch focusmonitor "HDMI-A-1"
-fi
+case "$1" in
+    "1")
+        if [[ "$CURRENT_MONITOR" == "HDMI-A-1" ]]; then
+            hyprctl dispatch focusmonitor "DP-3"
+        else
+            hyprctl dispatch focusmonitor "HDMI-A-1"
+        fi
+        ;;
+
+    # Logic for SUPER, 2 (Toggles between DP-1 and DP-3)
+    "2")
+        if [[ "$CURRENT_MONITOR" == "DP-1" ]]; then
+            hyprctl dispatch focusmonitor "DP-3"
+        else
+            hyprctl dispatch focusmonitor "DP-1"
+        fi
+        ;;
+esac
